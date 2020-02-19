0d7f064a1378ee7608b7fe0f370189f5431e6a11
A.Vihrogonov <vihrogonov@protonmail.com>
2020-02-19 16:09:31 +0000

    create class constructor & attach event listener

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Tooltip from './ui/tooltip';
+import Dropdown from './ui/dropdown';
+
+// create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
+
